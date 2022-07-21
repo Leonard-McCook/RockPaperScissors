a6bcce6afba7b7997b22a29d8b79ae583572f64f
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-07-21 11:45:35 -0400

    Add save game function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <script setup>
+import { ref, computed, onMounted } from 'vue';
 
+const wins = ref(0);
+const draws = ref(0);
+const losses = ref(0);
+
+const choice = ref(null)
+const computerChoice = ref(null)
+const verdict = ref(null)
+
+const outcomes = {
+  rock: {
+    rock: 'draw',
+    paper: 'loss',
+    scissors: 'win',
+  },
+  
+  paper: {
+    rock: 'win',
+    paper: 'draw',
+    scissors: 'loss',
+  },
+
+  scissors: {
+    rock: 'loss',
+    paper: 'win',
+    scissors: 'draw',
+  },
+
+}
+
+const play = c => {
+  choice.value = c;
+
+  const choices = ['rock', 'paper', 'scissors'];
+  const random = Math.floor(Math.random() * choices.length);
+  computerChoice.value = choices[random];
+
+  const outcome = outcomes[c][computerChoice.value]
+
+  if (outcome === 'win') {
+    wins.value++
+    verdict.value = "You win!"
+  } else if (outcome === 'loss') {
+    losses.value++
+    verdict.value = "You lose!";
+  } else {
+    draws.value++
+    verdict.value = "It's aDraw!";
+  }
+
+
+}
 </script>
 
 <template>
   <h1>Hello World</h1>
 </template>
+
